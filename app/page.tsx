59b186c78909da73b2e368c0e8c19f8c08be20a9
Alex Crooks <alex.crooks@Alex-CrooksBook.local>
2025-07-02 16:39:14 -0700

    Vastly improving hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Hero from "@/components/hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen pb-20 pt-20 gap-16 font-[family-name:var(--font-geist-sans)] space-y-5">
-      <Hero
-      />
+    <div className="items-center justify-items-center min-h-screen pb-20 pt-20 gap-16 font-[family-name:var(--font-geist-sans)] space-y-5"
+        style={{ background: 'linear-gradient(to bottom, #4e2761,#feddbc)' }}>
+            <div className="w-full p-4 rounded-t-lg flex flex-row items-center justify-evenly">
+            <p className="text-lg text-white">Blood.</p>
+            <p className="text-lg text-white">Sweat.</p>
+            <p className="text-lg text-white">Beer.</p>
+            </div>
+            <div className="w-full p-4 rounded-b-lg flex items-center justify-center">
+            <Image
+                src="/assets/transparent-rings.png"
+                alt="Beer Olympics Logo"
+                width={300}
+                height={200}
+            />
+        </div>
     </div>
   );
 }
